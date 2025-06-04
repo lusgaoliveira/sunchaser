21ebc35f4178a8a68c59f7837bcf2512971b2e20
Lucas Silva <lucassdeoliveira@outlook.com.br>
2025-06-03 22:25:49 -0300

    feat: demoon and skull

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -2,40 +2,52 @@ extends CharacterBody2D
 
 @onready var animation := $AnimatedSprite2D as AnimatedSprite2D
 
-const SPEED = 300.0
+const SPEED = 150.0
 
 var last_direction := ""
 
 func _physics_process(delta: float) -> void:
 	var input_vector := Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up"),
 	).normalized()
 
 	velocity = input_vector * SPEED
 	move_and_slide()
 
 	play_animation(input_vector)
+	
+	if Input.is_action_pressed("attack1"):
+		perform_attack()
 
 func play_animation(direction: Vector2) -> void:
 	if direction == Vector2.ZERO:
 		# Ainda nao tenho animacao parado
-		animation.stop()
+		last_direction = "stop"
 	else:
 		if direction.y > 0:
-			if direction.x > 0:
-				last_direction = "front_diagonal_right"
-			elif direction.x < 0:
-				last_direction = "front_diagonal_left"
-			else:
-				last_direction = "front"
+			last_direction = "front"
 		elif direction.y < 0:
-			if direction.x > 0:
-				last_direction = "fight_right"
-			elif direction.x < 0:
-				last_direction = "fight_left"
-			else:
-				last_direction = "back"
+			last_direction = "back"
+		elif direction.x > 0:
+			last_direction = "left"
+		else:
+			last_direction = "right"
 
 		animation.animation = last_direction
 		animation.play()
+
+func perform_attack() -> void:
+	print("Ataque realizado!")
+	match last_direction:
+		"front":
+			animation.animation = "load"
+		"back":
+			animation.animation = "attack2"
+		"left":
+			animation.animation = "attack2"
+		"right":
+			animation.animation = "attack2"
+		_:
+			animation.animation = "attack2"  
+	animation.play()    
---
+++ phases/phase_1/phase_1.gd
@@ -0,0 +1,63 @@
+extends Node2D
+class_name PhaseOne
+
+const _DIALOG_SCREEN: PackedScene = preload("res://phases/phase_1/dialogue1.tscn")
+
+var _dialog_data: Dictionary = {
+	0: {
+		"faceset": "res://sprites/characters/bartender/dialogue/alessander/alessander1.png",
+		"dialog": "Os demônios precisam deadead",
+		"title": "Padre Leusas"
+	},
+	1: {
+		"faceset": "res://sprites/characters/bartender/dialogue/alessander/alessander2.png",
+		"dialog": "Shai, precisamos damdoea suas ajudasd",
+		"title": "Padre Leusas"
+	},
+	2: {
+		"faceset": "res://sprites/characters/bartender/dialogue/alessander/alessander4.png",
+		"dialog": "Eles ao so cuados pelo assiantidoas da sua espoca",
+		"title": "Padre Leusas"
+	},
+	3: {
+		"faceset": "res://sprites/characters/bartender/dialogue/alessander/alessander3.png",
+		"dialog": "IREI ME VINGAR DESSESADAS D",
+		"title": "Shai"
+	},
+	4: {
+		"faceset": "res://sprites/characters/bartender/dialogue/shai/sha6.png",
+		"dialog": "Nenhum deles irá sobrar no mundo",
+		"title": "Shai"
+	},
+	5: {
+		"faceset": "res://sprites/characters/bartender/dialogue/shai/shai11.png",
+		"dialog": "Todos eles serão subjugados",
+		"title": "Shai"
+	},
+	
+	6: {
+		"faceset": "res://sprites/characters/bartender/dialogue/shai/shai14.png",
+		"dialog": "Todos eles serão subjugados",
+		"title": "Shai"
+	},
+	
+	7: {
+		"faceset": "res://sprites/characters/bartender/dialogue/shai/shai13.png",
+		"dialog": "Todos eles serão subjugados",
+		"title": "Shai"
+	},
+	
+	8: {
+		"faceset": "res://sprites/characters/bartender/dialogue/shai/shai15.png",
+		"dialog": "Todos eles serão subjugados",
+		"title": "Shai"
+	},
+}
+
+@export_category("Objects")
+@export var _hud: CanvasLayer = null
+
+func _ready() -> void:
+	var dialog_screen: DialogScreen = _DIALOG_SCREEN.instantiate()
+	dialog_screen.data = _dialog_data
+	_hud.add_child(dialog_screen)
